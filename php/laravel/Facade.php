ed81c3888f638561bf9850b1f9dd4d6ba5584af0
miiy <miiy@users.noreply.github.com>
2020-08-13 17:24:14 +0800

    Update

---
+++ php/laravel/Facade.php
@@ -0,0 +1,130 @@
+<?php
+
+use http\Exception\RuntimeException;
+
+/**
+ * Facade
+ *
+ * @see https://github.com/laravel/framework/blob/7.x/src/Illuminate/Support/Facades/Facade.php
+ */
+abstract class Facade
+{
+    /**
+     * The application instance being facaded.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected static $app;
+
+    /**
+     * The resolved object instances.
+     *
+     * @var array
+     */
+    protected static $resolvedInstance;
+
+    /**
+     * Get the root object behind the facade.
+     *
+     * @return mixed
+     */
+    public static function getFacadeRoot()
+    {
+        return static::resolveFacadeInstance(static::getFacadeAccessor());
+    }
+
+    /**
+     * Resolve the facade root instance from the container.
+     *
+     * @param  string|object  $name
+     * @return mixed
+     */
+    protected static function resolveFacadeInstance($name)
+    {
+        if (is_object($name)) {
+            return $name;
+        }
+
+        if (isset(static::$resolvedInstance[$name])) {
+            return static::$resolvedInstance[$name];
+        }
+
+        return static::$resolvedInstance[$name] = static::$app[$name];
+    }
+
+    /**
+     * Set the application instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public static function setFacadeApplication($app)
+    {
+        static::$app = $app;
+    }
+
+    /**
+     * Handle dynamic, static calls to the object.
+     *
+     * @param  string  $method
+     * @param  array   $args
+     * @return mixed
+     *
+     * @throws \RuntimeException
+     */
+    public static function __callStatic($method, $args)
+    {
+        $instance = static::getFacadeRoot();
+
+        if (! $instance) {
+            throw new RuntimeException('A facade root has not been set.');
+        }
+
+        return $instance->$method(...$args);
+    }
+}
+
+/**
+ * Route
+ *
+ * @see https://github.com/laravel/framework/blob/7.x/src/Illuminate/Support/Facades/Route.php
+ */
+class Route extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'router';
+    }
+}
+
+/**
+ * Router
+ */
+class Router
+{
+    public function get()
+    {
+        echo "get\n";
+    }
+
+    public function post()
+    {
+        echo "post\n";
+    }
+}
+
+
+Route::setFacadeApplication(['router' => new Router()]);
+Route::get();
+Route::post();
+
+/**
+# php facade.php
+get
+post
+ */
